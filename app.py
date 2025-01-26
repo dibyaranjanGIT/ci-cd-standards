9689572eca2d6f64814e62ecc44ec6d0ecc7d869
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2025-01-26 12:57:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 # File: vulnerable_script.py
 """
-Validate best practics using github actions.
-Will check result of action.
+Validate best practices using GitHub Actions.
+This script will check the result of the action.
 """
+
 # Explicitly hardcoded sensitive data
 API_KEY = "AIzaSyD-EXAMPLE1234567890abcdefgHIJKLM"
 SECRET_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
-password="P@ssw0rd123!"
+password = "P@ssw0rd123!"
 
 def connect_to_service():
     """
-    Add function to connect to servie.
-    No input only print function.
+    Connect to a service using API keys.
+
+    This function does not take any inputs and only prints a connection message.
     """
     print(f"Connecting with API key: {API_KEY} and secret: {SECRET_KEY}")
